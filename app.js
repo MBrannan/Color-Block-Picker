241769d481f8b6c9a96fd46f1e00e8c1abb41011
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-02 10:47:14 -0500

    Commited functional color picker

---
+++ app.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  var colorArray = ["Azure", "Chartreuse", "Gainsboro", "Peru", "Thistle"];
+  var random = randomNumber(0, colorArray.length - 1);
+
+  var randomColor = colorArray[random];
+
+  $('h2').append("<div class=random>Please select " + randomColor + "</div>");
+
+  $('.boxAzure').append("<div class='azure'></div>");
+  $('.boxChartreuse').append("<div class='chartreuse'></div>");
+  $('.boxGainsboro').append("<div class='gainsboro'></div>");
+  $('.boxPeru').append("<div class='peru'></div>");
+  $('.boxThistle').append("<div class='thistle'></div>");
+
+
+
+  $('.boxAzure').click(function() {
+    if ("Azure" == randomColor) {
+      alert("Correct!")
+    } else {
+      alert("Not quite!")
+    }
+  });
+
+  $('.boxChartreuse').click(function() {
+    if ("Chartreuse" == randomColor) {
+      alert("Correct!")
+    } else {
+      alert("Not quite!")
+    }
+  });
+
+  $('.boxGainsboro').click(function() {
+    if ("Gainsboro" == randomColor) {
+      alert("Correct!")
+    } else {
+      alert("Not quite!")
+    }
+  });
+
+  $('.boxPeru').click(function() {
+    if ("Peru" == randomColor) {
+      alert("Correct!")
+    } else {
+      alert("Not quite!")
+    }
+  });
+
+  $('.boxThistle').click(function() {
+    if ("Thistle" == randomColor) {
+      alert("Correct!")
+    } else {
+      alert("Not quite!")
+    }
+  });
+
+
+console.log("pls");
+
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
